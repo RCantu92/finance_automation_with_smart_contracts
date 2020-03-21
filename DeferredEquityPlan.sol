bb10610d49b7d0242683f5e14f131c8bfffa62ef
Roberto Cantu <robertocantu@Robertos-MacBook-Pro.local>
2020-03-21 15:46:38 -0500

    Updated commenting in DeferredEquityPlan

--- DeferredEquityPlan.sol
+++ DeferredEquityPlan.sol
@@ -1,59 +1,84 @@
 pragma solidity ^0.5.0;
 
-// lvl 3: equity plan
+/**
+ * @title Deferred Equity Plan
+ * This contract will be managing 1000 shares that will be distributed over
+ * 4 years to the employee. Ether won't be needed to work with this contract,
+ * as it will be storing and setting amounts that represent the number of
+ * distributed shares the employee owns and enforcing the vetting periods automatically.
+ */
 contract DeferredEquityPlan {
     
     address humanResources;
 
-    address payable employee; // bob
-    bool active = true; // this employee is active at the start of the contract
+    // Declaring an employee variable and a bool that states
+    // the employee is active at the start of the contract.
+    address payable employee;
+    bool active = true;
 
-    // @TODO: Set the total shares and annual distribution
+    // Total shares of 1000 and annual distribution of 25%.
     uint totalShares = 1000;
     uint annualDistribution = 250;
 
-    uint startTime = now; // permanently store the time this contract was initialized
+    // Permanently store the time this contract was initialized.
+    uint startTime = now;
     
-    // @TODO: Set the `unlock_time` to be 365 days from now
+    // Setting the unlock time to be 365 days from now.
     uint unlockTime = now + 365 days;
 
-    uint public distributedShares; // starts at 0
+    uint public distributedShares;
 
+    /**
+    * @dev The parameter employee address is passed to the previously
+    * declared employee address variable. The previously declared address
+    * for human resources is set to the msg.sender.
+    * @param _employee payable address of the employee
+    */
     constructor(address payable _employee) public {
         humanResources = msg.sender;
         employee = _employee;
     }
 
+    /**
+    * @dev Function that distributes the shares to the employee.
+    * The function only allows human resources to access it or
+    * the employee. It then verifies that enough time has passed
+    * to distribute the next 25% of shares.
+    */
     function distribute() public {
         require(msg.sender == humanResources || msg.sender == employee, "You are not authorized to execute this contract.");
         require(active == true, "Contract not active.");
 
-        // @TODO: Add "require" statements to enforce that:
-        // 1: `unlock_time` is less than or equal to `now`
-        // 2: `distributed_shares` is less than the `total_shares`
+        // Require statements to enforce that:
+        // 1: 'unlockTime' is less than or equal to 'now'
+        // 2: 'distributedShares' is less than the 'totalShares'
         require(unlockTime <= now, "It is not time to distributed your annual shares.");
         require(distributedShares < totalShares, "All of your shares have been distributed");
 
-        // @TODO: Add 365 days to the `unlock_time`
+        // Add 365 days to 'unlockTime'
         unlockTime = now + 365 days;
 
-        // @TODO: Calculate the shares distributed by using the function (now - start_time) / 365 days * the annual distribution
-        // Make sure to include the parenthesis around (now - start_time) to get accurate results!
+        // Calculate the shares distributed
         distributedShares = ((now - startTime) / 365 days) * annualDistribution;
 
-        // double check in case the employee does not cash out until after 5+ years
+        // Check in case the employee does not cash out until after 5+ years
         if (distributedShares > 1000) {
             distributedShares = 1000;
         }
     }
 
-    // human_resources and the employee can deactivate this contract at-will
+    /**
+    * @dev function allows human resources and the employee to deactivate this contract at-will.
+    */
     function deactivate() public {
         require(msg.sender == humanResources || msg.sender == employee, "You are not authorized to deactivate this contract.");
         active = false;
     }
 
-    // Since we do not need to handle Ether in this contract, revert any Ether sent to the contract directly
+    /**
+    * @dev The contract does not handle Ether, therefore, this function
+    * reverts any Ether sent to the contract directly.
+    */
     function() external payable {
         revert("Do not send Ether to this contract!");
     }
